60019a348631806361040c4b93116c64fac4439f
Umair Faisal <93310704+umairfasial@users.noreply.github.com>
2025-05-12 12:40:59 +0500

    new

---
+++ src/Screens/Extra.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Extra = () => {
+  return (
+    <View>
+      <Text>Extra</Text>
+    </View>
+  );
+};
+
+export default Extra;
+
+const styles = StyleSheet.create({});
