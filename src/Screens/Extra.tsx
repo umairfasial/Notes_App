7c0862879ee43a9def7b5fd9e2ca16035bebd1ab
Umair Faisal <93310704+umairfasial@users.noreply.github.com>
2025-06-12 17:06:45 +0500

    test

--- src/Screens/Extra.tsx
+++ src/Screens/Extra.tsx
@@ -23,9 +23,6 @@ const Extra = () => {
       <Text>Extra text here</Text>
       <Text>Extra text here</Text>
       <Text>Extra text here</Text>
-      <Text>Extra text here</Text>
-      <Text>Extra text here</Text>
-      <Text>Extra text here</Text>
     </View>
   );
 };
