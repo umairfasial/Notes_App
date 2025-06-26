d6978ff47283c9161170b64b56d1a3d9ef07f97a
Umair Faisal <93310704+umairfasial@users.noreply.github.com>
2025-06-26 14:40:04 +0500

    new add

--- src/Screens/Extra.tsx
+++ src/Screens/Extra.tsx
@@ -17,6 +17,15 @@ const Extra = () => {
       <Text>Extra text here</Text>
       <Text>Extra text here</Text>
       <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
+      <Text>Extra text here</Text>
     </View>
   );
 };
